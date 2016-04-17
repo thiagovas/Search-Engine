7c790bb4fae40b6189520c39baee5d72843190a4
Thiago Vieira de Alcantara Silva <thiagovas@cayman.lbd.dcc.ufmg.br.lbd.2.164.150.in-addr.arpa>
2016-04-17 01:03:23 -0300

    Now the crawler is adding the domains of the outbound links on the scheduler as well

--- CkSpider/src/crawler.cpp
+++ CkSpider/src/crawler.cpp
@@ -140,7 +140,7 @@ void Crawler::Crawl()
     }
     
     spider.Initialize(Utils::GetDomain(nextUrl).c_str());
-    spider.AddUnspidered(url.c_str());
+    spider.AddUnspidered(nextUrl.c_str());
     if(not spider.CrawlNext())
     {
       Crawler::scheduler_mutex.lock();
@@ -177,10 +177,12 @@ void Crawler::Crawl()
     for(int i = 0; i < spider.get_NumOutboundLinks(); i++)
     {
       if(not spider.GetOutboundLink(i, collectedUrl)) continue;
-      string curl = collectedUrl.getString();
+      string curl = collectedUrl.getString(), domain=Utils::GetDomain(curl);
       curl.shrink_to_fit();
+      domain.shrink_to_fit();
       
       Crawler::scheduler_mutex.lock();
+      Scheduler::AddURL(domain);
       Scheduler::AddURL(curl);
       Crawler::scheduler_mutex.unlock();
     }
