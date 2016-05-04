8e75266d29aeb09c7ee11a0f8582ee7b64608c82
Thiago Vieira <thiagovieiraas@gmail.com>
2016-05-04 14:35:19 -0300

    Added the initial skeleton of the indexer

---
+++ Indexer/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define endl "\n"
+typedef long long int ll;
+
+int main()
+{
+  ios::sync_with_stdio(false);  cin.tie(0);  cout.tie(0);
+  
+  
+  return 0;
+}
